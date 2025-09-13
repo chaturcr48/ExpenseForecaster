d9b1fa6b529bb5e4ee540779b4bb93e51168de56
Kumar Chatur Ranjan <chaturcr48@gmail.com>
2025-09-13 19:18:21 +0530

    Update forecast_service.py

--- api/forecast_service.py
+++ api/forecast_service.py
@@ -3,6 +3,7 @@ import pandas as pd
 from fastapi import FastAPI, Query
 from pathlib import Path
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from retrain_forecast import retrain_and_save
 
 app = FastAPI(title="Expense Forecaster Agent")
